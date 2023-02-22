eeea1eeb3748ac9cd6f0b52ec070aea566b240da
Barbara Fauth <bg.fauth@gmail.com>
2023-02-22 23:20:52 +0100

    update files according to the new tutorial

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # tiles
 
-This project is used for a first introduction to programming.  
+Get started with creative coding! A tutorial for beginners.
 Using Processing, we create a tiles pattern.  
+
+Please, feel free to head over to the [full tutorial](https://openprocessing.org/sketch/1816890).
 
 ![tiles](https://github.com/touch-Ada/fyraabe_coding_session/blob/main/img/tiles.jpg)
 
-For the course, we use https://openprocessing.org/sketch/create instead of downloading Processing. Thus, the JavaScript version is used instead of the default Processing language.
+For the course, we use [OpenProcessing](https://openprocessing.org/sketch/create) instead of downloading Processing. Thus, [p5js](https://p5js.org/reference/) is used instead of the default Processing language.
--- processing/tiles.js
+++ processing/tiles.js
@@ -1,46 +1,53 @@
 // draws a tiles pattern using diagonal lines
 
-// define variables
-sideLength = 100;
+// defines the size of the imaginary square in which the diagonal will be drawn
+const sideLength = 100; 
 
+// set the starting point of the pattern to the upper left corner
 xLeft = 0;
 xRight = sideLength;
 yTop = 0;
 yBottom = sideLength;
 
-// commands to run once
+/*
+The setup() function is executed once at the start of the program.
+It defines the initial settings, such as the canvas size or the background color.
+*/
 function setup() {
- createCanvas(windowWidth, windowHeight);
- background(0);
- stroke(255);
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	stroke(255);
 }
 
-// commands to run in an infinite loop
+/*
+The draw() function is executed infinitely once the setup() function is finished.
+It calls the drawRandomLine() function with the current values of the global variables.
+*/
 function draw() {
- drawRandomLine(xLeft, yTop, xRight, yBottom);
+	drawRandomLine(xLeft, yTop, xRight, yBottom);
 }
 
-// draw a line going randomly up or down
+// Randomly draws a diagonal from top to bottom or from bottom to top.
 function drawRandomLine(x1, y1, x2, y2) {
- randomNumber = random(10);
- inverted = randomNumber > 5;
- if (inverted) {
-  line(x1, y2, x2, y1);
- } else {
-  line(x1, y1, x2, y2);
- }
- shift();
+	randomNumber = random(10);
+	inverted = randomNumber < 5;
+	if (inverted) {
+		line(x1, y2, x2, y1);
+	} else {
+		line(x1, y1, x2, y2);
+	}
+	shift();
 }
 
-// move to the next drawing section
+// Moves the square where the diagonal is drawn one unit to the right or to a new line.
 function shift() {
- if (xRight < windowWidth) {
-  xLeft = xLeft + sideLength;
-  xRight = xRight + sideLength;
- } else {
-  xLeft = 0;
-  xRight = sideLength;
-  yTop = yTop + sideLength;
-  yBottom = yBottom + sideLength;
- }
+	if (xRight < windowWidth) {
+		xLeft = xLeft + sideLength;
+		xRight += sideLength;
+	} else {
+		xLeft = 0;
+		xRight = sideLength;
+		yTop += sideLength;
+		yBottom += sideLength;
+	}
 }
